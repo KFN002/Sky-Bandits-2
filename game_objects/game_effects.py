266014f84e77179fabdaf0b15f1900849f7eb09b
KFN001 <fedotovkirill4000@gmail.com>
2024-12-08 00:35:27 +0100

    adding pause

--- game_objects/game_effects.py
+++ game_objects/game_effects.py
@@ -1,9 +1,8 @@
 import random
-
 import pygame
 
 
-def render_glitch_text(text, font, color):
+def render_glitch_text(text, font, color='black'):
     base_text = font.render(text, True, color)
     offset_x, offset_y = random.randint(-5, 5), random.randint(-5, 5)
     glitch_text = pygame.Surface((base_text.get_width() + 10, base_text.get_height() + 10), pygame.SRCALPHA)
